30a03e10d616149431b759dc57fb6806ce9f8368
maverickjonesr-ux <maverick.jones.r@gmail.com>
2026-02-16 15:23:33 -0600

    Created My Scene
    Created MaverickScene, Created PlayerController Script, Created strarting position prefab, created a temporary player to test script, and Created new material Blue for temporary player character. Also added Prefab folder and Maverick Script folder. Created new tag Ground for later ground collision detection.

---
+++ Assets/Scripts/MavericksScripts/PlayerController_Test.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController_Test : MonoBehaviour
+{
+    public float moveSpeed = 30f;
+
+    Rigidbody rb;
+    Vector3 moveInput;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"); //A and D keys
+        float moveZ = Input.GetAxis("Vertical"); //W and S keys
+
+        moveInput = new Vector3(moveX, 0f, moveZ).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 playerPos = rb.position + moveInput * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(playerPos);
+    }
+}
