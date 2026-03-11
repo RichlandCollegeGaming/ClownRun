de34cf3aa79c13345d47ad7f27f28c6336e23494
maverickjonesr-ux <maverick.jones.r@gmail.com>
2026-03-11 18:46:48 -0500

    Multiplayer with different player prefabs
    Removed the old temp player prefabs
    Created new prefab with four player objects inside
    Edited the playerspawner script to add children as the player
    Added three materials red, green, yellow
    Deleted scripts from troubleshooting Multiplayer functionality

--- Assets/Scripts/MavericksScripts/PlayerSpawner.cs
+++ Assets/Scripts/MavericksScripts/PlayerSpawner.cs
@@ -33,9 +33,29 @@ public class PlayerSpawner : MonoBehaviour
             playerInput.transform.position = spawnPos;
         }
 
+        SetPlayerVisual(playerInput.transform, m_playerCount);
+
         //Register player with Camera
         cam.RegisterPlayer(playerInput.transform);
 
             m_playerCount++;
+    }
+
+    void SetPlayerVisual(Transform playerRoot, int playerIndex)
+    {
+        string[] names = { "Player1", "Player2", "Player3", "Player4" };
+
+        for(int i = 0; i < playerRoot.childCount; i++)
+        {
+            playerRoot.GetChild(i).gameObject.SetActive(false);
+        }
+
+        int visualIndex = playerIndex % names.Length;
+        Transform chosen = playerRoot.Find(names[visualIndex]);
+
+        if(chosen != null)
+        {
+            chosen.gameObject.SetActive(true);
+        }
     }
 }
